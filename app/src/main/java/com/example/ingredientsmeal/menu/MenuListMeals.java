d1ecef4b745eadc4db3d1a88889c79a34edc97d2
Bartek2 <bartek.machowski13@wp.pl>
2021-11-29 10:49:46 +0100

    #feature 002/Logowanie, Rejestracja, Zapomniałem Hasła, nowe toasty, nowe dialogi, połączenie z firebase

---
+++ app/src/main/java/com/example/ingredientsmeal/menu/MenuListMeals.java
@@ -0,0 +1,16 @@
+package com.example.ingredientsmeal.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ingredientsmeal.R;
+
+public class MenuListMeals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_list_meals);
+    }
+}
